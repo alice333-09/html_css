929228fb968cbd8f6b95c78fba1aedf3edad4401
rhavss <puupi169@gmail.com>
2025-10-21 08:21:46 -0300

    javva

---
+++ Atividades/ati1.js
@@ -0,0 +1,9 @@
+let num1 = prompt ("Diga um número: ")
+let num2 = prompt ("Diga um número: ")
+if(num1 > num2){
+    console.log(` ${num1} é maior que ${num2}`);
+} else if (num1 < num2) {
+    console.log(` ${num1} é menor que ${num2}`);
+} else {
+     console.log(` ${num1} é igual a ${num2}`);
+}
